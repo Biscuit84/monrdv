8011d5be4df63e3cc3bc41973f74f62b3c8a6c98
Achille Monod <achille.monod&@gmail.com>
2021-11-19 16:34:46 +0100

    modif home

--- monrdv-web/src/main/java/sopra/monrdv/web/HomeController.java
+++ monrdv-web/src/main/java/sopra/monrdv/web/HomeController.java
@@ -1,17 +1,13 @@
 package sopra.monrdv.web;
 
-import java.util.Date;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("")
-	public String home(Model model) {
-		model.addAttribute("dtJour", new Date());
+	public String home() {
 		return "home";
 	}
 }
